b757d1dce83638b28d7b48b4973bf1270fb3c910
blossomnell <blossom83@gmail.com>
2025-02-04 10:35:51 -0600

    initial commit

---
+++ src/test/java/Tests/tests.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class tests {
+ 
+	@Test
+    public void verifySetup() {
+        System.out.println("✅ TestNG is running successfully!");
+    }
+}
